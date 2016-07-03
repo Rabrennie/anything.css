70a8680232dfc60a506bec7741bc93f78fe6725a
Rab Rennie <Rabrennie@users.noreply.github.com>
2016-07-03 02:14:19 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "rennierab@gmail.com"
+
+git checkout master
+git add dist/anything.css
+git commit -m "Update dist [ci skip]"
+
+git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
